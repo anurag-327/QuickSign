3cd24613934c5dbcf184e508b27d445d2980bcb4
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-07 01:44:25 +0530

    Added responsiveness to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,12 +5,23 @@ import { motion } from "framer-motion"
 const Header = () => {
 
   const [drawer,setDrawer]=useState(false);
+  function toggleDrawer()
+  {
+     document.querySelector("#Drawer").classList.toggle("sm:hidden")
+     document.querySelector("#list").classList.toggle("sm:hidden")
+     document.querySelector("#X").classList.toggle("sm:hidden")
+  }
   return (
     <nav className=" fixed top-0 z-50 font-poppins bg-gradient-to-l from-[#111010] to-[#000000] via-[#181818] w-full max-w-screen py-3 px-6 text-white  ">
-      <div className='flex float-right sm:float-right mr-3 sm:mr-1  gap-8 sm:gap-3 text-lg'>
-        <a className='hover:underline hover:text-blue-200' href="/">Home</a>
+      <div className='flex float-right'>
+         <List id="list" className='hidden sm:block' onClick={toggleDrawer} size={32} weight="light" color="#ffffff" />
+         <X id="X" onClick={toggleDrawer} className="md:hidden lg:hidden xl:hidden 2xl:hidden sm:hidden" size={32} weight="light" color="#ffffff"/>
+      </div>
+      <div id="Drawer" className='flex float-right sm:text-center sm:float-none mr-3 gap-8 sm:gap-5  sm:px-10 text-lg sm:w-[300px] sm:absolute top-0 sm:left-0 sm:h-[100vh] sm:border-r sm:bg-[#181818] sm:hidden sm:flex-col '>
+        <a className='hover:underline hover:text-blue-200  sm:mt-52' href="/">Home</a>
         <a className='hover:underline hover:text-blue-200' href="/contact">Contact Us</a>
         <a className='hover:underline hover:text-blue-200' href="https://quicksign-doc.vercel.app/">Docs</a>
+        <a className='hover:underline hover:text-blue-200 px-2 py-1 border rounded-md text-center' href="https://my-vault-pm.vercel.app/">Try Now ↗</a>
       </div>
     </nav>
   )
--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -111,7 +111,7 @@ const LoginComponent = () =>
                   name="email" />
                <label
                  htmlFor="email"
-                 class="pointer-events-none absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+                 className="pointer-events-none absolute left-0 top-0 origin-[0_0] border border-solid border-transparent px-3 py-4 text-neutral-500 transition-[opacity,_transform] duration-200 ease-linear peer-focus:-translate-y-2 peer-focus:translate-x-[0.15rem] peer-focus:scale-[0.85] peer-focus:text-primary peer-[:not(:placeholder-shown)]:-translate-y-2 peer-[:not(:placeholder-shown)]:translate-x-[0.15rem] peer-[:not(:placeholder-shown)]:scale-[0.85] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
                  >Email address</label
                >
             </div>
