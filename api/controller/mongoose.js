d4f93c01403ffe7eec4776bc6001dabda24d736b
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-06 10:34:19 +0530

    Resolving Mongoose URL issue

--- api/controller/mongoose.js
+++ api/controller/mongoose.js
@@ -3,7 +3,7 @@ const dotenv=require('dotenv').config();
 mongoose.set("strictQuery", false);
 
 
-mongoose.connect(process.env.MONGO_URl)
+mongoose.connect("mongodb+srv://anurag-327:IZw3mlCF0WwjrIm7@cluster0.bg87uji.mongodb.net/QuickSign?retryWrites=true&w=majority")
 .then(()=>console.log("Database connected successully"))
 .catch(err => console.log("error setting up database",err))
 
