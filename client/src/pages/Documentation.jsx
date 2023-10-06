0b8af6e8b58634bd35987e570ff4fc4717cc4aca
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-10-06 22:45:18 +0530

    resolved docs positioning issues

--- client/src/pages/Documentation.jsx
+++ client/src/pages/Documentation.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Footer from '../components/Footer'
-
+import Header from '../components/Header'
 const documentation = () => {
   return (
-    <div className='flex justify-center items-center w-full h-screen'>
-      {/* <Header /> */}
-        <h2 className='font-bold tetx-5xl'>Documnetation section is coming soon</h2>
+    <div className='flex justify-center flex-col items-center w-full h-screen'>
+      <Header />
+        <h2 className='font-bold text-3xl text-white'>Documnetation section is coming soon</h2>
     <Footer />
     </div>
   )
