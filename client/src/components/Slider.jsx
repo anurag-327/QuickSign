cbd52b89cf8c493315e5d1e7b8956a92ca9c73c6
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-12-04 20:13:52 +0530

    Added light theme to dashboard

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,25 +1,87 @@
-import React from 'react'
-import { Files, Key, PlusCircle, SignOut, SquaresFour, TestTube, UserSquare } from 'phosphor-react'
-import { removeToken } from '../helper/tokenHandler'
-import { useNavigate } from 'react-router-dom'
-const Slider = ({tab}) => {
-    const navigate=useNavigate()
+import React from "react";
+import {
+  Files,
+  Key,
+  PlusCircle,
+  SignOut,
+  SquaresFour,
+  TestTube,
+  UserSquare,
+} from "phosphor-react";
+import { removeToken } from "../helper/tokenHandler";
+import { useNavigate } from "react-router-dom";
+const Slider = ({ tab }) => {
+  const navigate = useNavigate();
   return (
-    <div className='w-full md:w-[350px] flex flex-col gap-4 text-white  items-center py-4 px-3 bg-#181818] '>
-        <div>
-           <h2  className='font-bold'>DASHBOARD</h2>
-        </div>
-        <div className='flex flex-col   border-gray-500 py-4 gap-2 text-gray-400 text-start w-full px-2'>
-           <a className={`hover:bg-[#333] ${tab=="applications"&&"bg-[#333]"} flex gap-2 rounded-md px-2 items-center py-2`} href="/dashboard?tab=applications"><SquaresFour size={25} color="#ffffff" weight="light" />OAuth Apps</a>
-           <a className={`hover:bg-[#333] ${tab=="authorizedapps"&&"bg-[#333]"} flex gap-2 rounded-md px-2 items-center py-2`} href="/dashboard?tab=authorizedapps"><Key size={25} color="#ffffff" weight="light" />Authorized Apps</a>
-           <a className={`hover:bg-[#333] ${tab=="addoauth"&&"bg-[#333]"} flex gap-2 rounded-md px-2 items-center py-2`} href="/dashboard?tab=addoauth"><PlusCircle size={25} color="#ffffff" weight="light" />Add New OAuth App</a>
-           <a className={`hover:bg-[#333] ${tab=="profile"&&"bg-[#333]"} flex gap-2 rounded-md px-2 items-center py-2`} href="/dashboard?tab=profile"><UserSquare size={25} color="#ffffff" weight="light" />Profile</a>
-           <a className={`hover:bg-[#333]  flex gap-2 rounded-md px-2 items-center py-2`} href="https://quicksign-doc.vercel.app/"><Files size={25} color="#ffffff" weight="light" />Developer Docs</a>
-           <button onClick={() => {removeToken(); navigate("/")}} className=' hover:bg-[#333] outline-none flex gap-2 rounded-md px-2 items-center py-2 '><SignOut size={25} color="#ffffff" weight="light" />LogOut</button>
-           <a className={`hover:bg-[#333] border-2 border-zinc-700 flex justify-center mt-0 sm:mt-10 gap-2 rounded-md px-2 items-center py-2`} target='blank'  href="https://my-vault-pm.vercel.app/"><TestTube size={25} color="#ffffff" weight="light" />Try Now </a>
-        </div>
+    <div className="w-full md:w-[350px] flex flex-col gap-4 text-black items-center py-4 px-3 ">
+      <div>
+        <h2 className="font-bold ">DASHBOARD</h2>
+      </div>
+      <div className="flex flex-col border-gray-500 py-4 gap-2 text-black text-start w-full px-2">
+        <a
+          className={`hover:bg-gray-100 ${
+            tab == "applications" && "bg-gray-300 border border-gray-400"
+          } flex gap-2 rounded-md px-2 items-center py-2`}
+          href="/dashboard?tab=applications"
+        >
+          <SquaresFour size={25} color="#000000" weight="light" />
+          OAuth Apps
+        </a>
+        <a
+          className={`hover:bg-gray-100 ${
+            tab == "authorizedapps" && "bg-gray-300 border-gray-400"
+          } flex gap-2 rounded-md px-2 items-center py-2`}
+          href="/dashboard?tab=authorizedapps"
+        >
+          <Key size={25} color="#000000" weight="light" />
+          Authorized Apps
+        </a>
+        <a
+          className={`hover:bg-gray-100 ${
+            tab == "addoauth" && "bg-gray-300 border-gray-400"
+          } flex gap-2 rounded-md px-2 items-center py-2`}
+          href="/dashboard?tab=addoauth"
+        >
+          <PlusCircle size={25} color="#000000" weight="light" />
+          Add New OAuth App
+        </a>
+        <a
+          className={`hover:bg-gray-100 ${
+            tab == "profile" && "bg-gray-300 border-gray-400"
+          } flex gap-2 rounded-md px-2 items-center py-2`}
+          href="/dashboard?tab=profile"
+        >
+          <UserSquare size={25} color="#000000" weight="light" />
+          Profile
+        </a>
+        <a
+          className={`hover:bg-gray-100 border-gray-400 flex gap-2 rounded-md px-2 items-center py-2`}
+          href="https://quicksign-doc.vercel.app/"
+        >
+          <Files size={25} color="#000000" weight="light" />
+          Developer Docs
+        </a>
+        <button
+          onClick={() => {
+            removeToken();
+            navigate("/");
+          }}
+          className=" hover:bg-gray-100 border-gray-400 outline-none flex gap-2 rounded-md px-2 items-center py-2 "
+        >
+          <SignOut size={25} color="#000000" weight="light" />
+          LogOut
+        </button>
+        <a
+          className={`hover:bg-gray-300  border-2 bg-blue-400 border-zinc-700 flex justify-center mt-0 sm:mt-10 gap-2 rounded-md px-2 items-center py-2`}
+          target="blank"
+          href="https://my-vault-pm.vercel.app/"
+        >
+          <TestTube size={25} color="#000000" weight="light" />
+          Try Now{" "}
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
