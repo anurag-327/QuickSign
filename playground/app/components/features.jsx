201e6125a8fd97fdb8e3a1c4a4a9092019c3a6c8
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2024-03-23 00:33:17 +0530

    Redesigned playground

---
+++ playground/app/components/features.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="container px-4 py-4 mx-auto mt-10 border-gray-700">
+      <hr class="w-full h-0 border-t mt-4 border-gray-900 " />
+
+      <div className="flex justify-center w-full mt-20 sm:mt-20">
+        <span className=" px-4 text-base text-blue-100 relative z-10 before:content-[''] before:block before:relative before:inset-0 before:h-0.5 before:rounded-full before:bg-gradient-to-r from-transparent before:-left-36 before:top-3 to-gray-300 after:content-[''] after:block after:relative after:inset-0 after:h-0.5 after:bg-gradient-to-r  after:left-36 after:rounded-full after:rotate-180 after:-top-2.5 ">
+          Why QuickSign
+        </span>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-12 my-16 sm:flex-row sm:gap-2">
+        <div className="flex flex-col items-center justify-center rounded-lg shadow-md w-fit image-container">
+          <img
+            className="w-16 aspect-square image"
+            src="/SingleSignOn.webp"
+            alt="seamless authentication"
+          />
+          <h2 className="mt-1 font-sans text-xs text-gray-400 ">
+            Seamless Authentication
+          </h2>
+        </div>
+        <div>
+          <div className="relative h-px rotate-90 bg-gray-500 w-14 sm:w-20 sm:rotate-0">
+            <div className="absolute flex items-center justify-center w-6 h-6 text-gray-300 bg-transparent border border-gray-600 rounded-full -top-3 left-4 sm:left-7">
+              <span className="-mt-2.5">.</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center rounded-lg shadow-md w-fit image-container">
+          <img
+            className="w-16 image aspect-square"
+            src="/SocialLogin.webp"
+            alt="seamless authentication"
+          />
+          <h2 className="mt-1 font-sans text-xs text-gray-400 ">
+            User-Friendly Interface
+          </h2>
+        </div>
+        <div>
+          <div className="relative h-px rotate-90 bg-gray-500 w-14 sm:w-20 sm:rotate-0">
+            <div className="absolute flex items-center justify-center w-6 h-6 text-gray-300 bg-transparent border border-gray-600 rounded-full -top-3 left-4 sm:left-7">
+              <span className="-mt-2.5">.</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center rounded-lg shadow-md w-fit image-container">
+          <img
+            className="w-16 image aspect-square"
+            src="/Password.webp"
+            alt="seamless authentication"
+          />
+          <h2 className="mt-1 font-sans text-xs text-gray-400 ">
+            Minimal Data Storage
+          </h2>
+        </div>
+        <div>
+          <div className="relative h-px rotate-90 bg-gray-500 w-14 sm:w-20 sm:rotate-0">
+            <div className="absolute flex items-center justify-center w-6 h-6 text-gray-300 bg-transparent border border-gray-600 rounded-full -top-3 left-4 sm:left-7">
+              <span className="-mt-2.5">.</span>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center rounded-lg shadow-md w-fit image-container">
+          <img
+            className="w-16 image aspect-square"
+            src="/MagicLink.webp"
+            alt="seamless authentication"
+          />
+          <h2 className="mt-1 font-sans text-xs text-gray-400 ">
+            Robust Security
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
--- playground/app/page.js
+++ playground/app/page.js
@@ -1,17 +1,11 @@
 import { ShieldStar } from "@phosphor-icons/react/dist/ssr";
 import Image from "next/image";
+import Features from "./components/features";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <main className="relative flex flex-col items-center w-full min-h-screen py-6 bg-grid">
-      {/* shadow */}
-      {/* <div>
-        <div className="relative -mt-20">
-          <div className="spotlight rotate-[20deg]"></div>
-          <div className="spotlight"></div>
-          <div className="spotlight rotate-[-20deg]"></div>
-        </div>
-      </div> */}
       {/* Introducing  */}
       <div className="mt-20 sm:mt-10">
         <span className=" px-4 font-mono text-sm text-blue-100 relative z-10 before:content-[''] before:block before:relative before:inset-0 before:h-0.5 before:rounded-full before:bg-gradient-to-r from-transparent before:-left-28 before:top-4 to-gray-300 after:content-[''] after:block after:relative after:inset-0 after:h-0.5 after:bg-gradient-to-r  after:left-28 after:rounded-full after:rotate-180 after:-top-3 ">
@@ -21,7 +15,7 @@ export default function Home() {
       {/* Brand name  */}
       <div className="flex flex-col items-center gap-2 p-4 mt-8 rounded-full sm:flex-row ">
         <div>
-          <h2 className="py-2 text-6xl font-black text-center text-transparent sm:text-8xl sm:text-start bg-gradient-to-b from-blue-800 to-pink-200 bg-clip-text">
+          <h2 className="py-2 font-mono text-6xl font-black text-center text-transparent sm:text-8xl sm:text-start bg-gradient-to-b from-blue-800 to-pink-200 bg-clip-text">
             QuickSign
           </h2>
           <p className="mt-4 ml-0 font-mono text-center text-white whitespace-pre-wrap sm:ml-8 text-brandgray2 ">
@@ -31,11 +25,14 @@ export default function Home() {
       </div>
       {/* Try Now  */}
       <a
-        className="text-white hover:bg-blue-400 transition duration-100 border border-gray-600 mt-5 py-0.5 px-4 rounded-full"
+        className="px-8 py-1 mt-5 text-white transition duration-100 bg-gray-200 border border-gray-600 rounded-full bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 "
         href="/playground/auth"
       >
         Try Now
       </a>
+
+      {/* <Features /> */}
+      <Footer />
     </main>
   );
 }
@@ -43,22 +40,25 @@ export default function Home() {
 export function Navbar() {
   return (
     <>
-      <div className="flex items-center w-full mt-8 text-gray-400 justify-evenly ">
+      <div className="flex items-center justify-between w-full px-8 mt-8 text-gray-400 sm:justify-evenly">
         <div className="">
-          <a className="font-mono " href="https://quicksign-doc.vercel.app/">
+          <a
+            className="font-mono text-xs sm:text-base "
+            href="https://quicksign-doc.vercel.app/"
+          >
             View Docs
           </a>
         </div>
-        <a href="/" className="font-sans text-2xl font-bold">
+        <a href="/" className="font-sans font-bold">
           <ShieldStar
-            size={50}
+            size={40}
             weight="fill"
-            className="mt-2 sm:ml-16 text-branddark2 hover:text-branddark"
+            className="hidden ml-2 sm:block sm:ml-16 text-branddark2 hover:text-branddark"
           />
         </a>
         <a
           href="https://github.com/anurag-327/QuickSign"
-          className=" bg-blue-100 font-mono bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10  border py-0.5 px-4 border-gray-600 rounded-full"
+          className="sm:text-base text-xs bg-blue-100 font-mono bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10  border py-0.5 px-4 border-gray-600 rounded-full"
         >
           View on Github
         </a>
